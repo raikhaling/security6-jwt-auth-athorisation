5e90073715372c102fc885128378e858a26d2ba1
Nabin Rai <rainabinkhyal@gmail.com>
2023-08-14 15:42:55 +0545

    bug fix -> added missing annotation

--- src/main/java/com/khaling/springsecurityjwt/repository/UserRepository.java
+++ src/main/java/com/khaling/springsecurityjwt/repository/UserRepository.java
@@ -2,9 +2,10 @@ package com.khaling.springsecurityjwt.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.khaling.springsecurityjwt.entity.User;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
     //When you call the findByEmail method with an email parameter,
     // Spring Data JPA will create and execute a query to retrieve the user with the specified email.
